088c77d83f64a346fa1bcb5be98995831f32e2ff
henloboi <henloboii@protonmail.ch>
2020-08-31 18:57:15 +0800

    Clone tools

--- prep.sh
+++ prep.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt install brotli
 
+git clone https://github.com/xpirt/img2sdat tools/img2sdat
+git clone https://github.com/xpirt/sdat2img tools/sdat2img
+

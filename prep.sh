a52dc964378e00ccbfc2cb5d6561a46fe139123e
SudeepDuhoon <rishi.gothic@gmail.com>
2020-09-01 16:18:03 +0530

    hide tools folder, and ignore cloned tools
    Signed-off-by: SudeepDuhoon <rishi.gothic@gmail.com>

--- prep.sh
+++ prep.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt install brotli
 
-git clone https://github.com/xpirt/img2sdat tools/img2sdat
-git clone https://github.com/xpirt/sdat2img tools/sdat2img
+git clone https://github.com/xpirt/img2sdat .tools/img2sdat
+git clone https://github.com/xpirt/sdat2img .tools/sdat2img
 
